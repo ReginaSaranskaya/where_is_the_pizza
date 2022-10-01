b89f0bfb408a7cd4a1635563b29021f6d0e06b2e
Regina Saranskaya <r.saransckaya@gmail.com>
2022-10-01 21:57:24 +0400

    initial commit
    (cherry picked from commit 9fa57d4835cb800b16654c2864e008347243db25)

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+
+
+// const ingridients = [
+//   { name: 'cheese', text: "Моцарелла", price: "59" },
+//   { name: 'champignons', text: "Шампиньоны", price: "59" },
+//   { name: 'onion', text: "Красный лук", price: "59" },
+//   { name: 'pepper', text: "Сладкий перец", price: "59" },
+// ];
+
+function App() {
+  return (
+    <div className="App">
+      {/* <SectionTitle>Пицца</SectionTitle>
+      <SmallCardTitle>Супер Филадельфия</SmallCardTitle>
+      <CardTitle>EASY PEASY огуречный расколбас</CardTitle>
+      <Button>Оформить заказ</Button>
+      <Button size='small' variant='text'>Фильтры</Button>
+      <FilterIngridient id="check">Сырная смесь</FilterIngridient>
+      <Counter />
+      <RadioButton id="delivery" name="delivery">
+        Доставка
+      </RadioButton>
+      <RadioButton id="pickup" name="delivery">
+        Самовывоз
+      </RadioButton>
+      <FormsRadioButton id="payment" name="payment">
+        Наличными
+      </FormsRadioButton>
+      {ingridients.map(({name, text, price}) => <AdditionalIngredient name={name} text={text} price={price} />)}
+      <Price>от 399 ₽</Price>
+      <FormsInput type="text" subtext="Алексей" name="firstName">Имя*</FormsInput> */}
+    </div>
+  );
+}
+
+export default App;
